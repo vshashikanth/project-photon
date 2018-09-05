441b0a8fd029b14f6ba78fe5f226def60c00a60a
shashi <shashikanthv9@gmail.com>
2018-09-05 12:02:42 +0530

    commit 1

--- Photon/src/com/dmantz/shashikanth/One.java
+++ Photon/src/com/dmantz/shashikanth/One.java
@@ -2,6 +2,6 @@ package com.dmantz.shashikanth;
 
 public class One {
 public static void main(String... args) {
-	System.out.println("this is class One");
+	System.out.println("this class is under package com.dmantz.shashikanth of class One ");
 }
 }
